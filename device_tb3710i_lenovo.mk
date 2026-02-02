afb57ad7922adc47087176764a7d2347a12f29d2
Sreeranj Krishna M <sreeranj1000@gmail.com>
2026-02-02 21:05:39 +0530

    Fixed dalivk heap and recovery flashing issue

--- device_tb3710i_lenovo.mk
+++ device_tb3710i_lenovo.mk
@@ -138,4 +138,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
 # If you have 2GB RAM version, please use the 2048 dalvik-heap to fix wrong ram info after compile
 
 $(call inherit-product, build/target/product/full.mk)
-$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+$(call inherit-product, frameworks/native/build/tablet-7in-hdpi-1024-dalvik-heap.mk)
